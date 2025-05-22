1891e59959bcaa67f7a44193bab848c701b45f91
dxlliv <hello@dxlliv.dev>
2025-05-22 17:42:48 +0200

    refactor: Support multiple mimeType

--- runtime/components/Window/WindowVideoPlayer.vue
+++ runtime/components/Window/WindowVideoPlayer.vue
@@ -10,18 +10,44 @@ const { t } = useI18n()
 
 const videoUrl = loadVideoUrl(props.window.meta.path)
 
-function loadVideoUrl(fileHandle) {
-  if (!fileHandle) {
+function loadVideoUrl(pathString: string) {
+  if (!pathString) {
     return
   }
 
-  if (path.startsWith('http')) {
-    return path
+  if (pathString.startsWith('http')) {
+    return pathString
   }
 
-  const buffer = fs.readFileSync(fileHandle)
-  const blob = new Blob([buffer], { type: 'video/mp4' })
-  return URL.createObjectURL(blob)
+  try {
+    const buffer = fs.readFileSync(pathString)
+    const lastDotIndex = pathString.lastIndexOf('.')
+    const lastSlashIndex = pathString.lastIndexOf('/')
+    let ext = ''
+    if (lastDotIndex > lastSlashIndex) {
+      ext = pathString.substring(lastDotIndex).toLowerCase()
+    }
+
+    let mimeType
+
+    switch (ext) {
+      case '.mp4':
+        mimeType = 'video/mp4'
+        break
+      case '.webm':
+        mimeType = 'video/webm'
+        break
+      default:
+        console.warn(`File is not supported: ${ext}`)
+        return
+    }
+
+    const blob = new Blob([buffer], { type: mimeType })
+    return URL.createObjectURL(blob)
+  } catch (err) {
+    console.error('Error while loading the audio', err)
+    return
+  }
 }
 </script>
 
